80ff4cb4310a3bde3cf85936511610f0551f8c77
Vedant Mittal <vedantmittal14@gmail.com>
2026-07-11 06:08:40 +0530

    chore: update capacitor config to use production vercel url

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,8 +6,8 @@ const config: CapacitorConfig = {
   // webDir: 'out', // Not needed when pointing to live URL
   server: {
     // Change this to your live URL (e.g. Vercel) for the final production APK
-    // url: 'https://loomusapp.vercel.app',
-    url: 'http://192.168.29.45:3000', // Real Device over WiFi
+    url: 'https://loomusapp.vercel.app',
+    //url: 'http://192.168.29.45:3000', // Real Device over WiFi
     cleartext: true
   },
   plugins: {
